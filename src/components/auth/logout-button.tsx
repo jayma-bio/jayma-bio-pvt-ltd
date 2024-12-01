d8b46003a988b85ac72e58a48f788e26c9530a81
Suman Mondal <mondalsumanrock20@gmail.com>
2024-12-01 19:07:41 +0530

    Refactor LogoutButton component: remove unused router import and simplify logout functionality

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -1,23 +1,19 @@
 "use client";
 
 import { logout } from "@/actions/logout";
-import { useRouter } from "next/navigation";
 
 interface LogoutButtonProps {
   children?: React.ReactNode;
-};
+}
 
 export const LogoutButton = ({ children }: LogoutButtonProps) => {
-  const router= useRouter();
   const onClick = () => {
     logout();
-    window.location.reload();
-
-  }
+  };
 
   return (
     <span onClick={onClick} className="cursor-pointer">
       {children}
     </span>
-  )
-}
+  );
+};
