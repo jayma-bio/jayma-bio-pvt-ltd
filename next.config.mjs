a2f8f3a4ff1daa6f022a175ce13b4660feef2028
Atmajo <atmajoc@gmail.com>
2025-02-24 01:21:40 +0530

    Add custom headers for API routes in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-        },
-        {
-          protocol: "https",
-          hostname: "avatars.githubusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-        },
-        {
-          protocol: "https",
-          hostname: "lh3.googleusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "utfs.io",
-        },
-      ],
-    },
-    reactStrictMode: false,
-    pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  };
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Connection",
+            value: "keep-alive",
+          },
+          {
+            key: "Keep-Alive",
+            value: "timeout=60",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+  reactStrictMode: false,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+};
 
 export default nextConfig;
