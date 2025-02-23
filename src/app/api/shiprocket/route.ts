6f2742a46ba11986d6babe75456bef227d72bb68
Atmajo <atmajoc@gmail.com>
2025-02-24 00:06:37 +0530

    Refactor webhook handling to remove store ID retrieval and streamline order processing; update Shiprocket API integration to use axios instance

--- src/app/api/shiprocket/route.ts
+++ src/app/api/shiprocket/route.ts
@@ -1,14 +1,13 @@
-import axios from "axios";
+import { axiosinstance } from "@/lib/axios";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const token = await axios.post("/auth/login", {
+        const token = await axiosinstance.post("/auth/login", {
             email: process.env.SHIPROCKET_EMAIL,
             password: process.env.SHIPROCKET_PASSWORD,
         }).then((response) => {
             console.log("Logged in successfully:", response.data);
-
             return response.data.token;
         }).catch((error) => {
             console.error("Error:", error.response.data.message);
